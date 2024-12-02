8e6360dadfa754fe58351d9c9cde0ea73b4de0ae
Trevor Nielsen <trevor.kn@gmail.com>
2024-12-01 19:27:26 -0800

    Day 1, Part 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -18,7 +18,7 @@ fn main() {
 
     let mut sum = 0;
 
-    for (a, b) in zip(list1, list2) {
+    for (a, b) in zip(list1.iter(), list2.iter()) {
         sum += (a.parse::<i32>().unwrap() - b.parse::<i32>().unwrap()).abs();
     }
 
@@ -26,4 +26,36 @@ fn main() {
         "Part One: {:?}",
         sum
     );
+
+    // Part 2
+    let nums1 = list1.into_iter().map(|x| x.parse::<i32>().unwrap());
+    let nums2 = list2.into_iter().map(|x| x.parse::<i32>().unwrap());
+
+    let mut counts = Vec::new();
+
+    let mut last_num = -1;
+    let mut count = 0;
+
+    for num in nums2 {
+        if num != last_num {
+            counts.push((last_num, count));
+            println!("{:?}: {:?}", last_num, count);
+            count = 1;
+            last_num = num;
+        } else {
+            count += 1;
+        }
+    }
+
+    println!("Part Two: {:?}", nums1.map(|x|
+            {
+                for (n, c) in counts.iter() {
+                    if *n == x {
+                        return *n * *c;
+                    }
+                }
+
+                return 0;
+            }
+            ).sum::<i32>());
 }
