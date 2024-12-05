eb48e4ce921575fa0d1dcd9fff8bc4e6372717d6
Trevor Nielsen <trevor.kn@gmail.com>
2024-12-04 21:47:35 -0800

    Day 5, Part 2

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -32,6 +32,20 @@ fn check_order<T: Iterator<Item = i32> + Clone>(mut order: T, ord: &Ordinator) -
     }
 }
 
+fn aoc_sort(mut order: Vec<i32>, ord: &Ordinator) -> Vec<i32> {
+    for i in 0..order.len() - 1 {
+        for j in i + 1..order.len() {
+            if !ord.in_order((order[i], order[j])) {
+                let swp = order[i];
+                order[i] = order[j];
+                order[j] = swp;
+            }
+        }
+    }
+
+    order
+}
+
 fn main() {
     let input = fs::read_to_string("resources/day5.txt").expect("File path incorrect.");
 
@@ -53,16 +67,24 @@ fn main() {
     }
 
     let mut total = 0;
+    let mut total_part_2 = 0;
 
     for update in updates.lines() {
         let pages = update.split(',').map(|x| x.parse::<i32>().unwrap());
 
-        if check_order(pages.clone(), &ord) {
-            let pagelist = pages.collect::<Vec<i32>>();
+        let pagelist = pages.clone().collect::<Vec<i32>>();
 
+        if check_order(pages, &ord) {
+            // Part 1
             total += pagelist[pagelist.len() / 2];
+        } else {
+            // Part 2
+            let ordered = aoc_sort(pagelist, &ord);
+
+            total_part_2 += ordered[ordered.len() / 2];
         }
     }
 
     println!("Part 1: {:}", total);
+    println!("Part 2: {:}", total_part_2);
 }
