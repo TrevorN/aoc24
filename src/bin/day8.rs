2154ec1932745a6c46671f51517d87d1b3884997
Trevor Nielsen <trevor.kn@gmail.com>
2024-12-10 21:44:27 -0800

    Day 8, Part 2

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,3 +1,4 @@
+use gcd;
 use std::fs;
 
 fn produce_nodes<T: Iterator<Item = (char, i32, i32)>>(
@@ -17,6 +18,29 @@ fn produce_nodes<T: Iterator<Item = (char, i32, i32)>>(
             (2 * val.1 - i.1, 2 * val.2 - i.2),
             (2 * i.1 - val.1, 2 * i.2 - val.2),
         ])
+    })
+    .flatten()
+    .flatten()
+}
+
+fn produce_nodes_part2<T: Iterator<Item = (char, i32, i32)>>(
+    val: (char, i32, i32),
+    map: T,
+) -> impl Iterator<Item = (i32, i32)> {
+    map.map(move |i| {
+        if val.0 != i.0 {
+            return None;
+        }
+
+        if val.1 == i.1 && val.2 == i.2 {
+            return None;
+        }
+
+        let gcd = gcd::binary_u32((val.1 - i.1).abs() as u32, (val.2 - i.2).abs() as u32) as i32;
+        let x_slope = (val.1 - i.1) / gcd;
+        let y_slope = (val.2 - i.2) / gcd;
+
+        Some((-50..50).map(move |p| (val.1 + p * x_slope, val.2 + p * y_slope)))
     })
     .flatten()
     .flatten()
@@ -45,7 +69,7 @@ fn main() {
 
     let resonance = nodes
         .clone()
-        .map(|x| produce_nodes(x, nodes.clone()))
+        .map(|x| produce_nodes_part2(x, nodes.clone()))
         .flatten()
         .filter(|x| x.0 >= 0 && x.1 >= 0 && x.0 < x_dimm && x.1 < y_dimm);
 
