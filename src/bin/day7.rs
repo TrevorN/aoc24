5fb72c20c06bc99dc67e569d5b532996c541441e
Trevor Nielsen <trevor.kn@gmail.com>
2024-12-10 20:16:49 -0800

    Day 7, Part 1

---
+++ src/bin/day7.rs
@@ -0,0 +1,41 @@
+use rayon::prelude::*;
+use std::fs;
+
+fn operator_reduce<T: Iterator<Item = i64> + Clone>(
+    target: i64,
+    acc: i64,
+    mut remaining: T,
+) -> bool {
+    let new_val = remaining.next();
+
+    if let Some(x) = new_val {
+        operator_reduce(target, acc * x, remaining.clone())
+            || operator_reduce(target, acc + x, remaining.clone())
+    } else {
+        target == acc
+    }
+}
+
+fn part1(line: &str) -> i64 {
+    let mut s = line.split(":");
+    let result = s.next().unwrap().parse::<i64>().unwrap();
+
+    let mut arguments = s
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .map(|x| x.parse::<i64>().unwrap());
+
+    let first = arguments.next().unwrap();
+
+    result * operator_reduce(result, first, arguments) as i64
+}
+
+fn main() {
+    let input = fs::read_to_string("resources/day7.txt").expect("File path incorrect.");
+
+    println!(
+        "Part 1: {:}.",
+        input.as_parallel_string().lines().map(part1).sum::<i64>()
+    );
+}
