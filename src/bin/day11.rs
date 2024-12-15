5fd2a7b41d89fa989f9ab84c66104512b1939af4
Trevor Nielsen <trevor.kn@gmail.com>
2024-12-15 15:49:17 -0700

    Stack Friendly Day 11 Part 1

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,101 +1,83 @@
 use std::fs;
-use std::mem;
 
 // Lol, lets try doing a linked list.
-struct Stone {
-    value: i64,
-    next: Option<Box<Stone>>
+struct Stones {
+    values: Vec<i64>,
 }
 
-impl Stone {
+impl Stones {
     fn advance(&mut self) {
-        // Just update value if I'm zero.
-        if self.value == 0 {
-            self.value = 1;
-            if let Some(next) = &mut self.next {
-                next.advance();
-            }
-            return;
-        }
+        let mut index = 0;
 
-        // If my value has an even number of digits, split!
-        let strrep = self.value.to_string();
-        let strrep_len = strrep.len();
-
-        if strrep_len % 2 == 0 {
-            let first = strrep[..strrep_len/2].parse::<i64>().unwrap();
-            let second = strrep[strrep_len/2..].parse::<i64>().unwrap();
-
-            // Mutate self to become the first stone in the split.
-            self.value = first;
-          
-            if let Some(next) = &mut self.next {
-                next.advance();
+        loop {
+            if index >= self.values.len() {
+                return;
             }
 
-            let mut next = None;
-            mem::swap(&mut next, &mut self.next);
-            
-            let mut new_stone = Some(Box::new(Stone{ value: second, next: next}));
-            mem::swap(&mut self.next, &mut new_stone);
+            let v = self.values[index];
 
-            return;
-        }
+            if v == 0 {
+                self.values[index] = 1;
+                index += 1;
+                continue;
+            }
 
-        self.value *= 2024;
-        if let Some(next) = &mut self.next {
-            next.advance();
+            // If my value has an even number of digits, split!
+            let strrep = v.to_string();
+            let strrep_len = strrep.len();
+
+            if strrep_len % 2 == 0 {
+                let first = strrep[..strrep_len / 2].parse::<i64>().unwrap();
+                let second = strrep[strrep_len / 2..].parse::<i64>().unwrap();
+
+                // Mutate self to become the first stone in the split.
+                self.values[index] = first;
+                self.values.insert(index + 1, second);
+
+                index += 2;
+                continue;
+            }
+
+            self.values[index] *= 2024;
+            index += 1;
         }
     }
 
-    fn append(&mut self, tail: Box<Stone>) {
-        if let Some(n) = &mut self.next {
-            n.append(tail);
-        } else {
-            self.next = Some(tail);
-        }
+    fn append(&mut self, tail: i64) {
+        self.values.push(tail);
     }
 
     fn print(&self) {
-        print!("{} ", self.value);
-        if let Some(n) = &self.next {
-            n.print();
-            return;
+        for v in self.values.iter() {
+            print!("{}, ", v);
         }
         println!();
     }
 
     fn len(&self) -> i64 {
-        if let Some(n) = &self.next {
-            1 + n.len()
-        } else {
-            1
-        }
+        self.values.len() as i64
     }
 }
 
-fn parse_input(instr: &str) -> Stone {
-    let mut nums = instr.split_ascii_whitespace().map(|x| x.parse::<i64>()).flatten();
+fn parse_input(instr: &str) -> Stones {
+    let nums = instr
+        .split_ascii_whitespace()
+        .map(|x| x.parse::<i64>())
+        .flatten();
 
-    let mut result = Stone{ value: nums.next().expect("There will be at least one number in the input"), next: None };
-
-    for num in nums {
-        let next_stone = Box::new(Stone{value: num, next: None}); 
-        result.append(next_stone);
+    Stones {
+        values: nums.collect::<Vec<i64>>(),
     }
-
-    result
 }
 
 fn main() {
     let input = fs::read_to_string("resources/day11.txt").unwrap();
 
     let mut stones = parse_input(&input);
-    stones.print();
 
-    for _ in 0..25 {
+    for i in 0..75 {
         stones.advance();
-        stones.print();
+        println!("Completed blink: {}.", i);
     }
 
     println!("{}", stones.len());
