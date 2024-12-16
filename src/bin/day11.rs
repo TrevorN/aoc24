58c9e74e46ad06cc88fcc4497f2623333f393e4d
Trevor Nielsen <trevor.kn@gmail.com>
2024-12-15 22:36:27 -0700

    Day 11, Part 2

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,84 +1,89 @@
 use std::fs;
 
-// Lol, lets try doing a linked list.
-struct Stones {
-    values: Vec<i64>,
+struct Memory {
+    table: Vec<(i64, i64, i64)>,
 }
 
-impl Stones {
-    fn advance(&mut self) {
-        let mut index = 0;
+impl Memory {
+    fn new() -> Memory {
+        Memory { table: vec![] }
+    }
 
-        loop {
-            if index >= self.values.len() {
-                return;
+    fn recall(&self, height: i64, value: i64) -> Option<i64> {
+        for (h, v, total) in self.table.iter() {
+            if *h == height && *v == value {
+                println!("I WEMEMBER!");
+                return Some(*total);
             }
-
-            let v = self.values[index];
-
-            if v == 0 {
-                self.values[index] = 1;
-                index += 1;
-                continue;
-            }
-
-            // If my value has an even number of digits, split!
-            let strrep = v.to_string();
-            let strrep_len = strrep.len();
-
-            if strrep_len % 2 == 0 {
-                let first = strrep[..strrep_len / 2].parse::<i64>().unwrap();
-                let second = strrep[strrep_len / 2..].parse::<i64>().unwrap();
-
-                // Mutate self to become the first stone in the split.
-                self.values[index] = first;
-                self.values.insert(index + 1, second);
-
-                index += 2;
-                continue;
-            }
-
-            self.values[index] *= 2024;
-            index += 1;
         }
+
+        None
     }
 
-    fn append(&mut self, tail: i64) {
-        self.values.push(tail);
-    }
-
-    fn print(&self) {
-        for v in self.values.iter() {
-            print!("{}, ", v);
-        }
-        println!();
-    }
-
-    fn len(&self) -> i64 {
-        self.values.len() as i64
+    fn memorize(&mut self, height: i64, value: i64, total: i64) {
+        // I don't need to worry about duplicates because the calling functions would have already
+        // called "recall".
+        self.table.push((height, value, total));
     }
 }
 
-fn parse_input(instr: &str) -> Stones {
-    let nums = instr
+fn count_stones(height: i64, value: i64, mem: &mut Memory) -> i64 {
+    println!("h: {}, v: {}", height, value);
+    if height == 0 {
+        return 1;
+    }
+
+    if let Some(count) = mem.recall(height, value) {
+        return count;
+    }
+
+    if value == 0 {
+        let result = count_stones(height - 1, 1, mem);
+        mem.memorize(height, value, result);
+        return result;
+    }
+
+    let strval = value.to_string();
+    println!("{} -> {}", value, strval);
+    if strval.len() % 2 == 0 {
+        let first = strval[..strval.len() / 2].parse::<i64>().unwrap();
+        let second = strval[strval.len() / 2..].parse::<i64>().unwrap();
+        println!("{} -> {}, {}", value, first, second);
+
+        let first_result = count_stones(height - 1, first, mem);
+        let second_result = count_stones(height - 1, second, mem);
+        mem.memorize(height, value, first_result + second_result);
+        return first_result + second_result;
+    }
+
+    let result = count_stones(height - 1, value * 2024, mem);
+    mem.memorize(height, value, result);
+    result
+}
+
+fn parse_input(instr: &str) -> Vec<i64> {
+    instr
         .split_ascii_whitespace()
         .map(|x| x.parse::<i64>())
-        .flatten();
-
-    Stones {
-        values: nums.collect::<Vec<i64>>(),
-    }
+        .flatten()
+        .collect::<Vec<i64>>()
 }
 
 fn main() {
     let input = fs::read_to_string("resources/day11.txt").unwrap();
 
-    let mut stones = parse_input(&input);
+    let stones = parse_input(&input);
 
-    for i in 0..75 {
-        stones.advance();
-        println!("Completed blink: {}.", i);
-    }
+    println!("{:?}", stones);
 
-    println!("{}", stones.len());
+    let mut memory = Memory::new();
+
+    // Instead of breadth first, we want to go depth first!
+    println!(
+        "Total: {}",
+        stones
+            .iter()
+            .map(|x| count_stones(75, *x, &mut memory))
+            .sum::<i64>()
+    );
 }
